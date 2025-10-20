c543fc65aaebf4b54c4486a8272a9e7b74c194f7
Anderson Lima <andersonlimahw@gmail.com>
2025-10-20 16:04:41 -0300

    💾 [checkpoint] Unit tests

--- src/pages/Comunity/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const USERS_COLLECTION_NAME = 'Comunity-users';
-export const CONTACTS_COLLECTION_NAME = 'Comunity-contacts';
-export const MESSAGE_COLLECTION_NAME = 'Comunity-messages';
-
-// Final url:  Comunity/userId/contacts
-// Final url:  Comunity/userId/contacts/contactId/messages
